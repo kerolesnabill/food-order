ae1402657fe43fa96d864118076aabe5f34cb438
kerolesnabill <kerolesnabil513@gmail.com>
2022-07-26 17:52:32 +0200

    Edit MealCard component

--- src/components/UI/MealCard.jsx
+++ src/components/UI/MealCard.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const MealCard = ({ title, price, img }) => {
   return (
     <div className="text-white bg-neutral-700 shadow-2xl shadow-neutral-900 rounded-xl hover:scale-105 hover:shadow-red-900 transition-all w-10/12 mx-auto">
-      <img className="w-full h-auto rounded-t-xl" src={img} alt="" />
+      <img className="w-full h-[70%] rounded-t-xl" src={img} alt="" />
       <div className="p-4 grid grid-cols-2">
         <div>
-          <h3 className="text-3xl">{title}</h3>
-          <h4 className="text-xl text-yellow-500 pt-1">${price}</h4>
+          <h3 className="text-3xl font-bold md:text-2xl lg:text-xl">
+            {title}
+            <span className="text-xl text-yellow-500 pt-1"> ${price}</span>
+          </h3>
         </div>
         <div className="block text-center my-auto">
           <button className="bg-red-700 font-bold p-3 rounded-3xl h-12 w-10/12 hover:bg-red-800 transition-all">
