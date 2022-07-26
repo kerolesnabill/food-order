31b6531a5a14487fb9370dc5054c21f7a023d34d
kerolesnabill <kerolesnabil513@gmail.com>
2022-07-26 17:46:19 +0200

    Fetch meals from firebase

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMeals } from "../store/cartSlice";
 import MealCard from "./UI/MealCard";
 
 const Meals = () => {
+  const dispatch = useDispatch();
+  const { meals, isLoading } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(getMeals());
+  }, [dispatch]);
+
+  const mapMeals = meals.map((meal) => (
+    <MealCard
+      key={meal.id}
+      title={meal.title}
+      price={meal.price}
+      img={meal.img}
+    />
+  ));
   return (
-    <section className="container mx-auto grid grid-cols gap-y-12 mt-64 md:grid-cols-2 lg:grid-cols-4">
-      <MealCard />
+    <section className="container mx-auto my-64">
+      {isLoading ? (
+        <p className="text-center text-gray-300 text-xl align-middle">
+          Loading...
+        </p>
+      ) : !mapMeals.length > 0 ? (
+        <p className="text-center text-gray-300 text-xl">There is no Meals</p>
+      ) : (
+        <div className="grid grid-cols gap-y-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {mapMeals}
+        </div>
+      )}
     </section>
   );
 };
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,11 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const initialState = {};
+export const getMeals = createAsyncThunk(
+  "cart/getMeals",
+  async (arg, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const response = await fetch(
+        "https://food-order-react-website-default-rtdb.firebaseio.com/meals.json"
+      );
+      const data = await response.json();
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: data[key].id,
+          title: data[key].title,
+          price: data[key].price,
+          img: data[key].img,
+        });
+      }
+      return loadedMeals;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState,
+  initialState: { meals: [], isLoading: false, error: null },
   reducers: {},
+  extraReducers: {
+    // Get Meals
+    [getMeals.pending]: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [getMeals.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.meals = action.payload;
+    },
+    [getMeals.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
 });
 
 export default cartSlice.reducer;
